93c3ff90dce132acc000d29fec6304fdba1db661
Dyrhoi <realarcoz@gmail.com>
2021-06-16 05:32:03 +0200

    changed welcome

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,8 +2,8 @@ export const Welcome = () => {
 	return (
 		<div className="container">
 			<div className="content">
-				<h1>Hello World.</h1>
-				<p>This is the start of your project.</p>
+				<h1>Welcome to Wadog</h1>
+				<p>The little place to find dog walkers.</p>
 			</div>
 		</div>
 	);
